e9598efb43dc341a0b2012d654e34cd3d6953030
Alex Kaufman <alexkaufman06@gmail.com>
2015-02-16 13:46:52 -0800

    Update files

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,19 @@ $(document).ready(function() {
   $("form#new-to-do").submit(function(event) {
     event.preventDefault();
 
-    var newName = $("input#new-name").val();
-    var newDescription = $("input#new-description").val();
-    var newToDo = { name: newName, description: newDescription, status: false };
-    $("#todo").append("<li><span class='to-do-item'>" + newToDo.name + "</span></li>");
+    var newTask = $("#new-task").val();
 
-    $("input#new-name").val("");
-    $("input#new-description").val("");
+    $("#todo").append("<li>" + newTask + "</li>");
 
-    $(".to-do-item").last().click(function() {
-      $("#show-todo").show();
-      $("#show-todo h2").text(newToDo.name);
-      $(".description").text(newToDo.description);
+    $("#new-task").val("");
+
+
+    $("#todo").last().click(function(li) {
+      $(li.target).appendTo("#complete");
+
+      $("#complete").last().click(function(li) {
+        $(li.target).remove();
+      });
     });
   });
 });
