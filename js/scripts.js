51990eb3c2f05e5833ae8afaab0633760fd50c93
Ron Parkk <ronpark9@gmail.com>
2015-02-16 13:17:18 -0800

    Add to-do items but not be able to delete them

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("form#new-to-do").submit(function(event) {
+    event.preventDefault();
+
+    var newName = $("input#new-name").val();
+    var newDescription = $("input#new-description").val();
+    var newToDo = { name: newName, description: newDescription, status: false };
+
+    $("#todo").append("<li><span class='to-do-item'>" + newToDo.name + "</span></li>");
+
+    $("input#new-name").val("");
+    $("input#new-description").val("");
+
+    $(".to-do-item").last().click(function() {
+      $("#show-todo").show();
+      $("#show-todo h2").text(newToDo.name);
+      $(".description").text(newToDo.description);
+      debugger;
+    });
+    $(".complete").last().click(function() {
+
+    });
+  });
+});
